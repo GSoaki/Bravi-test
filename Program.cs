c26b96490de0a653d33471470b570f0a3d4f8d34
gabrielsoaki <soaki.ga@gmail.com>
2024-09-01 21:36:55 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static bool IsValid(string symbols)
+    {
+        Dictionary<char, char> matchingSupport = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { '}', '{' },
+            { ']', '[' }
+        };
+
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char symbol in symbols)
+        {
+            if (stack.Count > 0 && matchingSupport.ContainsKey(symbol))
+            {
+                char topElement = stack.Pop();
+                if (topElement != matchingSupport[symbol])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                stack.Push(symbol);
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(IsValid("(){}[]")); // true
+        Console.WriteLine(IsValid("[{()}](){}")); // true
+        Console.WriteLine(IsValid("[]{()")); // false
+        Console.WriteLine(IsValid("[{)]")); // false
+    }
+}
